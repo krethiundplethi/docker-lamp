18663ebb0b6c50580ca841b6c3bcbe7e4a815aa0
krethiundplethi <krethiundplethi@gmail.com>
2019-09-08 21:40:55 +0200

    added nexcloud and other configs

---
+++ supporting_files/config.php
@@ -0,0 +1,26 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocphhevq23jj',
+  'passwordsalt' => 'Q/P0Pf+JnMM72VFz9n7flI54sJdiOy',
+  'secret' => 'H1dgm6xP0LRQH7g/tMdP9E0t19jb9efQUQU8+m/kgNApC+Ps',
+  'trusted_domains' =>
+  array (
+    0 => 'cloud.fellnhofer.engineering',
+  ),
+  'datadirectory' => '/data',
+  'dbtype' => 'mysql',
+  'version' => '16.0.4.1',
+  'overwrite.cli.url' => 'https://cloud.fellnhofer.engineering',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'oc_admin1000',
+  'dbpassword' => '9J/iAq7qWfPROoLKALH5LARnM9wukk',
+  'installed' => true,
+  'theme' => '',
+  'loglevel' => 2,
+  'maintenance' => false,
+);
+
